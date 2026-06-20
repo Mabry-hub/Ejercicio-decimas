facccbebcf9d50f6b753ac3fdd49ed6a9f8cc346
Mabry-hub <mau.mardones@duocuc.cl>
2026-06-20 18:47:50 -0400

    Archivo base
    no lo quise ingresar vacío, así que le puse unas cuantas validaciones, junto con notas y empecé a avanzar la opción 1, pero falta trabajo aún.

---
+++ Ejercicio decimas.py
@@ -0,0 +1,47 @@
+#funciones validaciones
+def validar_codigo(codigo):    #validar el codigo sin espacios
+    return codigo.strip()!=""
+
+def buscar_codigo(reservas, codigo):    #las funciones de validacion de codigo-(que no tenga espacios) y buscar el codigo-(si es que ya existe) pueden fusionarse en una sola funcion
+    for reserva in reservas:
+        if reserva["codigo"]==codigo:
+            return None
+
+def validar_nombre(nombre):
+    return nombre.strip()!=""
+
+#funciones opciones y funcionales
+
+def mostrar_menu():
+    print("1. Registrar reserva")
+    print("2. Buscar reserva")
+    print("3. Actualizar reserva")
+    print("4. Eliminar reserva")
+    print("5. Mostrar reservas")
+    print("6. Mostrar estadísticas")
+    print("7. Salir")
+
+#OPCION 1
+def registrar_reserva(reservas):
+    codigo=input("Ingrese codigo de reserva: ").strip()
+
+    if not validar_codigo(codigo):
+        print("Error: El código no puede estar vacio")
+        return
+    if buscar_codigo(reservas, codigo) is not None:
+        print("Error: El código de la reserva ya existe")
+
+    nombre=input("ingrese nombre del huésped: ").strip()
+    if not validar_nombre(nombre):
+        print("Error: el nombre no puede estar vacío")
+        return
+    
+    try:
+        noches=int(input("Ingrese la cantidad de noches de estadía:"))
+        if noches<=0:
+            print("Error: La cantidad ingresada tiene que ser mayor a 0.")
+            return
+    except ValueError:
+        print("Error: ", ValueError)
+        return
+    
