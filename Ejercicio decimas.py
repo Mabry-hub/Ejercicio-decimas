507b404025516ff2c4af9b8afd340c1223021256
HUGO-CAR <hu.carrascoa@duocuc.cl>
2026-06-21 20:31:58 -0400

    Add files via upload

--- Ejercicio decimas.py
+++ Ejercicio decimas.py
@@ -2,14 +2,24 @@
 def validar_codigo(codigo):    #validar el codigo sin espacios
     return codigo.strip()!=""
 
-def buscar_codigo(reservas, codigo):    #las funciones de validacion de codigo-(que no tenga espacios) y buscar el codigo-(si es que ya existe) pueden fusionarse en una sola funcion
-    for reserva in reservas:
-        if reserva["codigo"]==codigo:
-            return None
-
 def validar_nombre(nombre):
     return nombre.strip()!=""
 
+def buscar_codigo(reservas, codigo):    #las funciones de validacion de codigo-(que no tenga espacios) y buscar el codigo-(si es que ya existe) pueden fusionarse en una sola funcion
+    for i in range(len(reservas)):
+        if reservas["codigo"]==codigo:
+            return i
+    return -1
+
+def calcular_categoria(total):
+    if total<200000:
+        return 'Económica'
+    elif total <=500000:
+        return 'Estándar'
+    else: 
+        return 'Premium'
+
+    
 #funciones opciones y funcionales
 
 def mostrar_menu():
@@ -21,33 +31,112 @@ def mostrar_menu():
     print("6. Mostrar estadísticas")
     print("7. Salir")
 
-#OPCION 1
-def registrar_reserva(reservas):
-    codigo=input("Ingrese codigo de reserva: ").strip()
 
-    if not validar_codigo(codigo):
-        print("Error: El código no puede estar vacio")
-        return
-    if buscar_codigo(reservas, codigo) is not None:
-        print("Error: El código de la reserva ya existe")
 
-    nombre=input("ingrese nombre del huésped: ").strip()
-    if not validar_nombre(nombre):
-        print("Error: el nombre no puede estar vacío")
-        return
-    
+#===OPCION===
+
+def leer_opcion():
     try:
-        noches=int(input("Ingrese la cantidad de noches de estadía:"))
-        if noches<=0:
-            print("Error: La cantidad ingresada tiene que ser mayor a 0.")
-            return
+        opcion=int(input('*INGRESE UNA OPCIÓN DEL 1 AL 7:'))
+        
+        if opcion<=7 and opcion>=1:
+            print('*Opción válida*')
+            return opcion
+        
+        else:
+            return 0 
+        
     except ValueError:
-        print("Error: ", ValueError)
+        print('ERROR.Debe ingresar un número entero con las opciones del menú...*')
+        return 0
+
+
+
+
+
+#**************
+
+def eliminar_reserva(reservas):
+    print('\n*ELIMINAR RESERVA*')
+    codigo=input('*Ingrese el código a eliminar...:').strip()
+    
+    if not validar_codigo(codigo):
+        print('*El código no puede estar vacío...*')
         return
     
+    posicion=buscar_codigo(reservas,codigo)
 
+    if posicion != -1:
 
+        eliminada=reservas.pop(posicion)
+        print(f'¡La reserva {eliminada['nombre']}-{codigo},fue eliminada...  ')
+
+    else:
+        print('*ERROR*,el codigo ingresao no existe...')
+
+#===mostrar reservas===
+
+def mostrar_reservas(reservas):
+    print('*MOSTRAR RESERVAS*')
     
+    if len(reservas)== 0:
+        print('*no hay reservas ingresadas en el sistema ')
+        return 
+    
+    for i, reserva in enumerate(reservas):
+
+        print('*'*30)
+        print(f'*Posición...:{i}')
+        print(f'*Código...:{reservas["codigo"]}')
+        print(f'*Huesped...:{reservas["nombre"]}')
+        print(f'*Noches...:{reservas["noches"]}')
+        print(f'*Valor/Noche...:{reservas["valor_noche"]}')
+        print(f'*Total...:{reservas["tota"]}')
+        print(f'*Categoria...:{reservas["categoria"]}')
+        print('*'*30)
+
+
+
+#===MOSTRAR ESTADISTICAS===
+
+def mostrar_estadisticas(reservas):
+    print('\n***Estadisticas***')
+    cant_total=len(reservas)
+    if cant_total==0:
+        print('*NO HAY RESERVAS REGISTRADAS PARA GENERAR ESTADISTICAS*')
+        return
+    
+    ingresos_totales=0
+    reserva_mayor=reservas[0]
+
+    for reserva in reservas:
+        ingresos_totales*=reserva['total']
+        if reserva["total"]>reserva_mayor["total"]:
+            reserva_mayor=reserva
+
+    promedio_ingresos =ingresos_totales/cant_total
+
+    print(f'*Cantidad total de reservas:{cant_total}')
+    print(f'*Ingresos totales de hotel:${ingresos_totales}')
+    print(f'*Promedio de ingresos por reserva:${promedio_ingresos}')
+    print(f'*Reserva de mayor valor: ')
+    print(f'**Código:{reserva_mayor["codigo"]}')
+    print(f'**Nombre:{reserva_mayor["nombre"]}')
+    print(f'**Valor:{reserva_mayor['total']}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+#===MAIN===
     
 def main():
 
@@ -57,8 +146,7 @@ def main():
 
         mostrar_menu()
 
-        opcion=leer_opcion(reservas)
-
+        opcion=leer_opcion()
         if opcion==1:
             registrar_reserva(reservas)
 
@@ -82,4 +170,28 @@ def main():
             break
         
 main()
+
+
+
+
+
+
+
+
+#===ELIMINAR RESERVA===
+
+def eliminar_reserva()
+    
+    print('\n*ELIMINAR RESERVA*')
+
+    codigo=input('*Ingrese el código de la reserva a eliminar...:')
+
+    if not validar_codigo(codigo):
+
+        print('*El código está vacío*')
+        return 0
+    
+    if codigo in reservas
+
+
 
